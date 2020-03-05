ebba3ab6cadde8b4955e4b51cda71f9e24773f9a
jamalka85b15 <crutchfield.nur@gmail.com>
2020-03-04 20:50:15 -0600

    Open each Link and go back

---
+++ src/test/java/com/automation/tests/day4/VerifyThatElementISGone.java
@@ -0,0 +1,24 @@
+package com.automation.tests.day4;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class VerifyThatElementISGone {
+    public static void main(String[] args) throws Exception {
+        WebDriverManager.chromedriver().version("79").setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/multiple_buttons");
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("disappearing_button")).click();
+        Thread.sleep(2000);
+
+        if(driver.findElements(By.id("disappearing_button")).size()==0){
+            System.out.println("TEST PASSED");
+        }else{
+            System.out.println("TEST FAILED");
+        }
+    }
+}
