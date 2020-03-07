fd8341d6fbdc28e744bc495844305d1945471b84
jamalka85b15 <crutchfield.nur@gmail.com>
2020-03-07 11:51:43 -0600

    RadioButtonTest

---
+++ src/test/java/com/automation/tests/day5/RadioButtonTest.java
@@ -0,0 +1,27 @@
+package com.automation.tests.day5;
+
+import com.automation.tests.BrowserUtilities;
+import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class RadioButtonTest {
+    public static void main(String[] args) {
+        WebDriver driver= DriverFactory.createADriver("chrome");
+        driver.get("http://practice.cybertekschool.com/radio_buttons");
+        BrowserUtilities.wait(2);
+        WebElement blackbutton=driver.findElement(By.id("black"));
+        if(blackbutton.isDisplayed() && blackbutton.isEnabled()){
+            System.out.println("CLICKED ON BLACK BUTTON");
+            blackbutton.click();
+        }else{
+            System.out.println("FAILED CLICK ON BALCK BUTTON");
+        }
+        if(blackbutton.isSelected()){
+            System.out.println("TEST PASSED");
+        }else{
+            System.out.println("TEST FAILED");
+        }
+    }
+}
