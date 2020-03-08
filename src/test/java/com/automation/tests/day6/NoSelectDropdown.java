b54e7e3066819a9764ef3f1da98989c7df43f8c9
jamalka85b15 <crutchfield.nur@gmail.com>
2020-03-08 12:40:42 -0500

    NoSelectDropDown

---
+++ src/test/java/com/automation/tests/day6/NoSelectDropdown.java
@@ -0,0 +1,30 @@
+package com.automation.tests.day6;
+
+import com.automation.tests.BrowserUtilities;
+import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class NoSelectDropdown {
+    public static void main(String[] args) {
+        WebDriver driver = DriverFactory.createADriver("chrome");
+        driver.get("http://practice.cybertekschool.com/dropdown");
+        BrowserUtilities.wait(3);
+        driver.findElement(By.id("dropdownMenuLink")).click();
+        BrowserUtilities.wait(2);
+
+//        driver.findElement(By.linkText("Google")).click();
+        List<WebElement> allLinks=driver.findElements(By.className("dropdown-item"));
+        for(WebElement link: allLinks){
+            System.out.println(link.getText()+ " : "+ link.getAttribute("href"));
+        }
+
+        BrowserUtilities.wait(2);
+        driver.quit();
+    }
+
+
+}
