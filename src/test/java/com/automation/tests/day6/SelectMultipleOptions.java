4a13653b8a73cf94875a22d04d0ace66d6d47bb1
jamalka85b15 <crutchfield.nur@gmail.com>
2020-03-08 11:47:16 -0500

    SelectMultipleOptions

--- src/test/java/com/automation/tests/day6/SelectMultipleOptions.java
+++ src/test/java/com/automation/tests/day6/SelectMultipleOptions.java
@@ -1,4 +1,27 @@
 package com.automation.tests.day6;
 
+import com.automation.tests.BrowserUtilities;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 public class SelectMultipleOptions {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().version("79").setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/dropdown");
+        BrowserUtilities.wait(3);
+        Select languagesSelect=new Select(driver.findElement(By.name("Languages")));
+
+        boolean isMultiuple=languagesSelect.isMultiple();
+        System.out.println(isMultiuple);
+
+
+
+        BrowserUtilities.wait(3);
+        driver.quit();
+    }
+
 }
