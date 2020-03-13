2978ac7e1dc3f2f433792317667dc4997b10e183
jamalka85b15 <crutchfield.nur@gmail.com>
2020-03-12 22:22:05 -0500

    Xpath

--- src/test/java/com/automation/tests/day7/Xpath.java
+++ src/test/java/com/automation/tests/day7/Xpath.java
@@ -2,14 +2,21 @@ package com.automation.tests.day7;
 
 import com.automation.tests.BrowserUtilities;
 import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class Xpath {
+    static String userNameLocator="//label[text()='Username']/following-sibling::input";
+    static  String passwordLocator="//label[text()='Password']/following-sibling::input";
+    static String LoginBntLocator="//button[contains(text(), 'Login')]";
     public static void main(String[] args) {
 
         WebDriver driver= DriverFactory.createADriver("chrome");
         driver.get("http://practice.cybertekschool.com/login");
         BrowserUtilities.wait(3);
+        driver.findElement(By.xpath(userNameLocator)).sendKeys("tomsmith");
+        driver.findElement(By.xpath(passwordLocator)).sendKeys("SuperSecretPassword");
+        driver.findElement(By.xpath(LoginBntLocator)).click();
 
 
         BrowserUtilities.wait(3);
---
+++ src/test/java/com/automation/tests/officehours/SeleniumOH2.java
@@ -0,0 +1,37 @@
+package com.automation.tests.officehours;
+
+import com.automation.tests.BrowserUtilities;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumOH2 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().version("79").setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://automationpractice.com");
+        WebElement searchBox=driver.findElement(By.id("search_query_top"));
+        searchBox.sendKeys("tshirt"+ Keys.ENTER);
+        WebElement error=driver.findElement(By.xpath("//p[@class='alert alert-warning']"));
+        BrowserUtilities.wait(2);
+        String errorText =error.getText();
+        System.out.println("Error message "+ errorText);
+
+        searchBox=driver.findElement(By.id("search_query_top"));
+        searchBox.clear();
+        searchBox.sendKeys("t-shirt"+ Keys.ENTER);
+        BrowserUtilities.wait(2);
+        WebElement count=driver.findElement(By.className("product-count"));
+        System.out.println("itemcount "+ count.getText());
+
+        WebElement addToCart= driver.findElement(By.className("button ajax_add_to_cart_button btn btn-default"));
+        addToCart.click();
+
+
+
+        driver.quit();
+    }
+}
