bd29d81302aad15dd2af5a7cba56aaf557a643bf
jamalka85b15 <crutchfield.nur@gmail.com>
2020-03-17 14:29:41 -0500

    PracticeTests

--- src/test/java/com/automation/tests/vytrack/fleet/VehiclePageTests.java
+++ src/test/java/com/automation/tests/vytrack/fleet/VehiclePageTests.java
@@ -1,6 +1,6 @@
 package com.automation.tests.vytrack.fleet;
-
 import com.automation.tests.BrowserUtilities;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -12,52 +12,69 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-
-import java.util.List;
-
 public class VehiclePageTests {
-    private WebDriver driver;
-    private String URL="https://qa2.vytrack.com/user/login";
-    private String username="storemanager85";
-    private String password="UserUser123";
+    private String URL = "https://qa2.vytrack.com/user/login";
+    //    CREDENTIALS FOR store manager
+    private String username = "storemanager85";
+    private String password = "UserUser123";
     private By usernameBy = By.id("prependedInput");
     private By passwordBy = By.id("prependedInput2");
-    private By fleetBy=By.xpath("//span[@class='title title-level-1' and contains(text(), 'Fleet')]");
-    private By subtitleBy=By.className("oro-subtitle");
+    private By fleetBy = By.xpath("//span[@class='title title-level-1' and contains(text(),'Fleet')]");
+    private By subtitleBy = By.className("oro-subtitle");
+    private By pageNumber =By.cssSelector("input[type='number']");
+    private WebDriver driver;
 
+    //****************************************************************
 
-
-@Test
+    @Test
     public void verifyPageSubTitle(){
-    driver.findElement(usernameBy).sendKeys(username);
-    driver.findElement(passwordBy).sendKeys(password, Keys.ENTER);
-    BrowserUtilities.wait(5);
-    driver.findElement(fleetBy).click();
-    BrowserUtilities.wait(2);
 
-    driver.findElement(By.linkText("Vehicles")).click();
-    WebElement subtitleElement =driver.findElement(subtitleBy);
-    System.out.println(subtitleElement.getText());
-   
+        //find subtitle element
+        WebElement subTitleElement = driver.findElement(subtitleBy);
+        System.out.println(subTitleElement.getText());
+        String expected="All Cars";
+        String actual=subTitleElement.getText();
+        Assert.assertEquals(actual,expected);
+    }
 
+    //*********************************************************
 
-}
+    @Test(description = "Verify page is equal to '1' ")
+            public void verifyPageEqualToOne() {
+    String expected ="1";
+    String actual=driver.findElement(pageNumber).getAttribute("value");
+    Assert.assertEquals(expected,actual);
+    }
+
+    //**********************************************************
 
     @BeforeMethod
-    public void setup(){
+    public void setup() {
         WebDriverManager.chromedriver().version("79").setup();
-        driver=new ChromeDriver();
+        driver = new ChromeDriver();
         driver.get(URL);
         driver.manage().window().maximize();
+        driver.findElement(usernameBy).sendKeys(username);
+        driver.findElement(passwordBy).sendKeys(password, Keys.ENTER);
+        BrowserUtilities.wait(5);
+        driver.findElement(fleetBy).click();
+        BrowserUtilities.wait(2);
 
+        driver.findElement(By.linkText("Vehicles")).click();
+
+        BrowserUtilities.wait(5);
     }
+
+    //*************************************************************
+
     @AfterMethod
     public void teardown() {
+        //if webdriver object alive
         if (driver != null) {
+            //close browser, close session
             driver.quit();
+            //destroy webdriver object for sure
             driver = null;
         }
-
-
     }
 }
