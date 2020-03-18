829db69929d515966f4f73dd590e01a2c4a13474
jamalka85b15 <crutchfield.nur@gmail.com>
2020-03-18 18:47:08 -0500

    ActionsTest

--- src/test/java/com/automation/tests/day9/ActionsTest.java
+++ src/test/java/com/automation/tests/day9/ActionsTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -25,13 +26,18 @@ public class ActionsTest {
     public void hoverOnImage(){
         driver.get("http://practice.cybertekschool.com/hovers");
         BrowserUtilities.wait(5);
-        WebElement img1=driver.findElement(By.xpath("//img[1]"));
-        WebElement img2=driver.findElement(By.xpath("//img[2]"));
-        WebElement img3=driver.findElement(By.xpath("//img[3]"));
+        WebElement img1=driver.findElement(By.xpath("(//img)[1]"));
+        WebElement img2=driver.findElement(By.xpath("(//img)[2]"));
+        WebElement img3=driver.findElement(By.xpath("(//img)[3]"));
         //build() needs when you have couple actions
         //always finish with perform()
         actions.moveToElement(img1).pause(1000)
-                .moveToElement(img2).build().perform();
+                .moveToElement(img2).pause(1000)
+                .moveToElement(img3).build().perform();
+        BrowserUtilities.wait(3);
+        actions.moveToElement(img1).perform();
+       WebElement imgText1 = driver.findElement(By.xpath("//h5[text()='name: user1']"));
+        Assert.assertTrue(imgText1.isDisplayed());
     }
 
 
