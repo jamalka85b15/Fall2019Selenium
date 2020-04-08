9d02885bf0255a2a7d3aeebe0b8d579f38691e21
jamalka85b15 <crutchfield.nur@gmail.com>
2020-04-07 23:31:48 -0500

    BrowserUtilities-Screenshot method added

--- src/test/java/com/automation/utilities/BrowserUtilities.java
+++ src/test/java/com/automation/utilities/BrowserUtilities.java
@@ -1,26 +1,93 @@
 package com.automation.utilities;
 
-import org.openqa.selenium.WebElement;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BrowserUtilities {
-    public static void wait(int seconds){
-        try{
-            Thread.sleep(1000*seconds);
-        }catch(InterruptedException e){
-            e.printStackTrace();
 
+    public static void wait(int seconds) {
+        try {
+            Thread.sleep(1000 * seconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
-    public static List<String> getTextFromWebElements(List<WebElement>elements) {
+
+    /**
+     * @param elements represents collection of WebElements
+     * @return collection of strings
+     */
+    public static List<String> getTextFromWebElements(List<WebElement> elements) {
         List<String> textValues = new ArrayList<>();
         for (WebElement element : elements) {
-            textValues.add(element.getText());
+            if (!element.getText().isEmpty()) {
+                textValues.add(element.getText());
+            }
         }
-        return  textValues;
+        return textValues;
     }
 
+    /**
+     * waits for backgrounds processes on the browser to complete
+     *
+     * @param timeOutInSeconds
+     */
+    public static void waitForPageToLoad(long timeOutInSeconds) {
+        ExpectedCondition<Boolean> expectation = driver -> ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete");
+        try {
+            WebDriverWait wait = new WebDriverWait(Driver.getDriver(), timeOutInSeconds);
+            wait.until(expectation);
+        } catch (Throwable error) {
+            error.printStackTrace();
+        }
+    }
 
+    /**
+     * Clicks on an element using JavaScript
+     *
+     * @param element
+     */
+    public static void clickWithJS(WebElement element) {
+        ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
+        ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].click();", element);
+    }
+
+    /**
+     * Scroll to element using JavaScript
+     *
+     * @param element
+     */
+    public static void scrollTo(WebElement element) {
+        ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    /**
+     * @param name screenshot name
+     * @return path to the screenshot
+     */
+    public static String getScreenshot(String name) {
+        String path = System.getProperty("user.dir")+ "/test-output/screenshots/"+name+"png";
+        TakesScreenshot takesScreenshot = (TakesScreenshot) Driver.getDriver();
+        File source =takesScreenshot.getScreenshotAs(OutputType.FILE);
+        //where file will be saved
+        File destination= new File(path);
+        try{
+            //copy file to the previously specified location
+            FileUtils.copyFile(source,destination);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return path;
+    }
 }
--- src/test/java/com/automation/utilities/DateTimeUtilities.java
+++ src/test/java/com/automation/utilities/DateTimeUtilities.java
@@ -1,7 +1,9 @@
 package com.automation.utilities;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class DateTimeUtilities {
     //This methos will return current date as a String
@@ -10,7 +12,13 @@ public class DateTimeUtilities {
     //MMM - to specify month like: Jan, Feb, Mar
     //dd - to specify day like 01,02,03
     //yyy - to specify year like 2010,2020
-    public static String getCurrentDate(String format){
-        return LocalDate.now().format(DateTimeFormatter.ofPattern("MMM dd, yyyy"));
+    public static String getCurrent0Date(String format){
+        return LocalDate.now().format(DateTimeFormatter.ofPattern(format));
+    }
+
+    public static long getTimeDifference(String start, String end, String format){
+        LocalTime startTime= LocalTime.parse(start, DateTimeFormatter.ofPattern(format));
+        LocalTime endTime= LocalTime.parse(end, DateTimeFormatter.ofPattern(format));
+        return ChronoUnit.HOURS.between(startTime,endTime);
     }
 }
